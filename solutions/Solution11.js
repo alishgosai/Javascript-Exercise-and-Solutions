32c2e316d97711d3d310e1e750da97c8ddc5657a
Alish Pawn <alishpau@gmail.com>
2024-08-03 07:08:20 +0545

    solution 11

--- solutions/Solution11.js
+++ solutions/Solution11.js
@@ -1,4 +1,19 @@
 
 // Solution for Question 11
 
+function getAvailableBooks(books) {
+    return books
+      .filter(book => book.isAvailable) // Filter books that are available
+      .map(book => book.title);          // Map to get an array of titles
+  }
+  
+  // Example usage:
+  const books = [
+    { title: 'Book 1', isAvailable: true },
+    { title: 'Book 2', isAvailable: false },
+    { title: 'Book 3', isAvailable: true },
+    { title: 'Book 4', isAvailable: false }
+  ];
+  
+  console.log(getAvailableBooks(books)); // Output: ['Book 1', 'Book 3']
   
