48e43e29122eb9f3abef142e31a4ea9d83de4447
AlishG <alishgosai@gmail.com>
2024-08-03 14:47:45 +1000

    solution 12

--- solutions/Solution12.js
+++ solutions/Solution12.js
@@ -1,4 +1,13 @@
-
 // Solution for Question 12
 
-  
+function formatMessages(message, timestamp) {
+  const date = new Date(timestamp);
+
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  return `${message} (${hours}: ${minutes}:${seconds});`;
+}
+
+console.log(formatMessages('Hello', 1021281083000208))
