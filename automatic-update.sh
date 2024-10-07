910b81f12cff234e61cc198a4a63d0a68a2eace0
ChickenBro <@alishgosai>
2024-10-07 22:02:06 +1100

    add

--- automatic-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install chokidar-cli globally if not already installed
-# npm install -g chokidar-cli
-
-# Use chokidar to watch for changes in the questions directory
-
-# chokidar 'questions/*.md' -c 'node update-questions.js'
