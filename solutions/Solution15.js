6de6121d4a64ab32a18e11ed2711930d0eb5ad86
ChickenBro <@alishgosai>
2024-10-07 20:58:02 +1100

    add solution 15

--- solutions/Solution15.js
+++ solutions/Solution15.js
@@ -1,4 +1,30 @@
-
 // Solution for Question 15
 
-  
+function calculateTotalExpenses(expenses) {
+  let total = 0; // to keep track of total amount
+
+  for (let i = 0; i < expenses.length; i++) {
+    total += expenses[i].amount;
+  }
+
+  return total;
+}
+
+console.log(
+  calculateTotalExpenses([
+    { date: "2024-01-01", amount: 50 },
+    { date: "2024-02-02", amount: 40 },
+  ])
+);//90
+
+//ANOTHER WAY
+
+const calculateTotalExpenses2 = (expenses) =>
+  expenses.reduce((total, expense) => total + expense.amount, 0);
+
+console.log(
+  calculateTotalExpenses2([
+    { date: "2024-01-01", amount: 50 },
+    { date: "2024-02-02", amount: 40 },
+  ])
+);//90
