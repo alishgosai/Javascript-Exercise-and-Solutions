f1442973248d66a94b7c84039006dba2072aa00b
ChickenBro <@alishgosai>
2024-10-08 18:05:56 +1100

    add quesiton 16

--- solutions/Solution16.js
+++ solutions/Solution16.js
@@ -1,4 +1,14 @@
-
 // Solution for Question 16
 
-  
+function searchContacts(contacts, searchTerm) {
+  return contacts.filter((contacts) =>
+    contacts.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+}
+
+const contactList = [
+  { name: "Alice", phoneNumber: "1234567890" },
+  { name: "Bob", phoneNumber: "0987654321" },
+];
+
+console.log(searchContacts(contactList, "Ali")); //[ { name: 'Alice', phoneNumber: '1234567890' } ]
